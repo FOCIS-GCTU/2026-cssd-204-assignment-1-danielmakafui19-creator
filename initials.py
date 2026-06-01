af15c92af7e9897be194df166d7a4b9462fde6cb
Daniel Makafui <2425402350@live.gctu.edu.gh>
2026-05-31 21:47:58 -0800

    Submission

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Printing out the letters in my initials using a python program
+# Assignment Number: 2
+#
+# Name: Daniel Makafui Anku
+# STUDENT ID: 2425402350
+# Email: 2425402350@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+#  On my honour, Daniel Makafui Anku, this programming assignment is my own work
+# and I have not provided this code to any other student
+
 
 def main():
-  pass #code goes here
+    # Print my initials
 
+    print()
+    print("...DMA")
+    print()
+
+    print("...DDDDDDDDDDDD........MM........MM........AAAAAAAAAAAA.....")
+    print("...DD........DD........MMM......MMM........AA........AA.....")
+    print("...DD........DD........MMMM....MMMM........AA........AA.....")
+    print("...DD........DD........MM.MM..MM.MM........AA........AA.....")
+    print("...DD........DD........MM..MMMM..MM........AAAAAAAAAAAA.....")
+    print("...DD........DD........MM...MM...MM........AA........AA.....")
+    print("...DD........DD........MM........MM........AA........AA.....")
+    print("...DD........DD........MM........MM........AA........AA.....")
+    print("...DD........DD...**...MM........MM...**...AA........AA...**")
+    print("...DDDDDDDDDDDD...**...MM........MM...**...AA........AA...**")
+
+    print()
 
 
 main()
